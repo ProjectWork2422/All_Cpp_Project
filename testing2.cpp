e901533c9f36a94a322041c3b0356f5f7d826e3f
ProjectWork2422 <app.it3213@gmail.com>
2026-09-23 15:06:35 +0530

    Implement modulus operation in testing2.cpp
    Added modulus operation and its output condition.

--- testing2.cpp
+++ testing2.cpp
@@ -9,6 +9,7 @@ int main(){
     int d = a - b;
     int m=a * b;
     float u=a/b;
+    int l=a%b;
 
 
     if(c>0){
@@ -18,6 +19,9 @@ int main(){
         cout<<"sum is less than zero which is invalid :"<<c<<endl;
     }
     cout<<"This are the operations  excepts the   addition of two numbers"<<endl;
+    if(a>b){
+    cout<<"The reminder  of the two number is "<<l<<endl;
+    }
     cout<<"The difference of two number is : "<<d<<endl;
     cout<<"The multiplication of two number is : "<<m<<endl;
     cout<<"The dividation of two  numbers is : "<<u<<endl;
