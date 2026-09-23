a288abf08ffeccb73eb9c52abff1129b33e594f8
projectwork2422 <app.it3213@gmail.com>
2026-09-23 12:28:58 +0530

    ADDITION F THE  TWO NUMBER   PROGRAM

---
+++ testing2.cpp
@@ -0,0 +1,12 @@
+#include<istream>
+#include<ostream>
+using namespace std;        
+int main(){
+    int a,b;            
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;      
+    /*addition of the numbers*/
+    int c = a + b;
+    cout<<"The sum is: "<<c<<endl;
+    return 0;
+}
